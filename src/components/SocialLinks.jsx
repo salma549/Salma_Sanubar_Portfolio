f4c986727ee23a526b618da8ed08a9a3fcf32fc8
salma549 <salmasanubar786@gmail.com>
2024-08-10 09:50:33 +0530

    Salma Sanubar Portfolio

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/salmaResume.pdf",
+      href: "/Salma_Sanubar_Portfolio.pdf",
       style: "rounded-br-md",
       download: true,
     },
